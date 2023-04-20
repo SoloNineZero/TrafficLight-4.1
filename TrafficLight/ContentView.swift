e0141d4b1820b3d720b5c1aae9a2577a7cf4842c
SoloNineZero <solodyankin.ie@ya.ru>
2023-04-20 23:54:04 +0500

    Добавлена логика программы

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -12,6 +12,11 @@ enum Light {
 }
 
 struct ContentView: View {
+    @State private var title = "Start"
+    
+    let onLight = 1.0
+    let offLight = 0.2
+    
     @State private var redLight = 0.2
     @State private var yellowLight = 0.2
     @State private var greenLight = 0.2
@@ -28,14 +33,35 @@ struct ContentView: View {
                     .padding(20)
             }
             Spacer()
-            SwitchLightButton(title: "Start", action: getOnColor)
+            SwitchLightButton(title: title, action: getOnColor)
         }
         .padding(50)
     }
     private func getOnColor() {
+        if title == "Start" {
+            title = "Next"
+        }
         
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            redLight = onLight
+            yellowLight = offLight
+            greenLight = offLight
+        case .yellow:
+            currentLight = .green
+            redLight = offLight
+            yellowLight = onLight
+            greenLight = offLight
+        case .green:
+            currentLight = .red
+            redLight = offLight
+            yellowLight = offLight
+            greenLight = onLight
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
